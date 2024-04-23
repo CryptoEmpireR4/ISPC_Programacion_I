47ae3becf9bf0323f48ece7aadb542302e04bf34
Luciano Adolfo Cañas <79887578+CryptoEmpireR4@users.noreply.github.com>
2024-04-23 11:46:39 -0300

    Update Desafio 1.py

--- Desafio 1.py
+++ Desafio 1.py
@@ -1,6 +1,6 @@
 #Desafio 1 dar vuelta un numero de tres digitos
 numero=input("Inserte un número entero de tres digitos: ")
-while int(len(numero))!=3:
+while int(len(numero))!=3 or not numero.isnumeric(): #Determina si no tiene 3 cifras y no es numerico
     print("vuelta a intentar ingresar un numero de 3 cifras")
     numero=input("Inserte un número entero de tres digitos: ")
 if int(len(numero))==3:
