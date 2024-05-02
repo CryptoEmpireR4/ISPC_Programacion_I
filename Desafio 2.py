ec41fd7eec8cf1f09aa78dc7fd04a54b5aaf768b
Luciano Adolfo Cañas <79887578+CryptoEmpireR4@users.noreply.github.com>
2024-05-02 13:13:16 -0300

    Delete Desafio 2.py

--- Desafio 2.py
+++
@@ -1,5 +0,0 @@
-#Desafio 2: Contar Horas
-hora=int(input("Inserte cual es la hora actual: "))
-contar=int(input("Inserte las horas a contar desde la hora actual: "))
-horario=(hora+contar) % 24
-print("Pasando "+str(contar)+" desde la hora actual, el reloj daria que son las "+str(horario)+" horas.")
